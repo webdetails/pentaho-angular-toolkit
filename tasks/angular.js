68a58c007dc352d439cac585bfef99ee70253f79
francisco-guilherme <francisco.guilherme@webdetails.pt>
2015-11-18 13:02:36 +0000

    Add code vet and angular optimization tasks

---
+++ tasks/angular.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var $	 			= require('../gulp.config.js');
+var gulp			= require('gulp');
+var angularFilesort	= require('gulp-angular-filesort');
+var concat			= require('gulp-concat');
+
+/**
+ * Optimize angular files
+ * Move to dist folder
+ *
+ * @return {Stream}
+ */
+ module.exports = function() {
+	 $.log('Optimizing angular files');
+
+ 	return gulp
+	 	.src([$.dev_path + $.module.name + '/**/*.js'])
+	 	.pipe(angularFilesort())
+	 	.pipe(concat($.module.name + '.js'))
+	 	.pipe(gulp.dest($.dist_path));
+ };
