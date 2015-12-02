16a6b5a56e0698c7f274b4bc81af410274f7b50e
francisco-guilherme <francisco.guilherme@webdetails.pt>
2015-12-02 17:00:56 +0000

    Updated file pattern selection

--- karma.conf.js
+++ karma.conf.js
@@ -2,11 +2,16 @@
 
 module.exports = function(config) {
   config.set({
+
     browsers: ['PhantomJS'],
+
     frameworks: ['jasmine'],
+
     files: [
-      'src/*.js',
-      'src/*.spec.js'
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'src/pentaho-angular-toolkit/**/*.module.js',
+      'src/pentaho-angular-toolkit/**/*.js'
     ]
   });
 };
