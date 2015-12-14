cc43b84b35f1984a4767fc927ed13f8cb717bed4
francisco-guilherme <francisco.guilherme@webdetails.pt>
2015-12-14 12:20:41 +0000

    Update commit and push info

--- _scripts/deploy-doc.sh
+++ _scripts/deploy-doc.sh
@@ -2,7 +2,7 @@
 set -e # exit with nonzero exit code if anything fails
 
 # go to the module directory and create a *new* Git repo
-cd "$GH_REPO"
+cd doc
 git init
 #git remote add origin "https://github.com/$GH_USER/$GH_REPO"
 
@@ -15,11 +15,11 @@ git config user.email "pentaho@pentaho.com"
 # the first and only commit to this new Git repo contains all the
 # files present with the commit message.
 git add .
-git commit -m "Pentaho Angular Toolkit Module $TRAVIS_BRANCH"
+git commit -m "Pentaho Angular Toolkit Doc $TRAVIS_BRANCH"
 #git tag -a "$TRAVIS_BRANCH" -m "Release of version $TRAVIS_BRANCH"
 
 # force push from the current repo's master
 # (all previous history will be lost, since we are overwriting it.)
 # we redirect any output to /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://$GH_TOKEN@github.com/$GH_USER/$GH_REPO.git" master:module > /dev/null 2>&1
+git push --force --quiet "https://$GH_TOKEN@github.com/$GH_USER/$GH_REPO.git" master:gh-pages > /dev/null 2>&1
 #git push --tags "https://$GH_TOKEN@github.com/$GH_USER/$GH_REPO.git"
