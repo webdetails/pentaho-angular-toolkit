be66e00726c01f433423b7712096d8be3dfa6060
francisco-guilherme <francisco.guilherme@webdetails.pt>
2015-12-14 12:28:18 +0000

    Clean deploy scripts

--- _scripts/deploy-doc.sh
+++ _scripts/deploy-doc.sh
@@ -4,9 +4,6 @@ set -e # exit with nonzero exit code if anything fails
 # go to the module directory and create a *new* Git repo
 cd doc
 git init
-#git remote add origin "https://github.com/$GH_USER/$GH_REPO"
-
-#git checkout --orphan module
 
 # inside this git repo we'll pretend to be a new user
 git config user.name "pentaho"
@@ -16,10 +13,8 @@ git config user.email "pentaho@pentaho.com"
 # files present with the commit message.
 git add .
 git commit -m "Pentaho Angular Toolkit Doc $TRAVIS_BRANCH"
-#git tag -a "$TRAVIS_BRANCH" -m "Release of version $TRAVIS_BRANCH"
 
 # force push from the current repo's master
 # (all previous history will be lost, since we are overwriting it.)
 # we redirect any output to /dev/null to hide any sensitive credential data that might otherwise be exposed.
 git push --force --quiet "https://$GH_TOKEN@github.com/$GH_USER/$GH_REPO.git" master:gh-pages > /dev/null 2>&1
-#git push --tags "https://$GH_TOKEN@github.com/$GH_USER/$GH_REPO.git"
