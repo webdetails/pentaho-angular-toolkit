5710d3942cbb5b664c2ce88de1fae1d7d158663c
francisco-guilherme <francisco.guilherme@webdetails.pt>
2015-12-14 10:26:45 +0000

    Updated module branch repo configurations

--- _scripts/deploy-module.sh
+++ _scripts/deploy-module.sh
@@ -4,7 +4,10 @@ set -e # exit with nonzero exit code if anything fails
 # go to the module directory and create a *new* Git repo
 cd "$GH_REPO"
 git init
-git remote add origin "https://github.com/$GH_USER/$GH_REPO-module.git"
+git remote add origin "https://github.com/$GH_USER/$GH_REPO"
+
+git checkout module
+git rm -rf .
 
 # inside this git repo we'll pretend to be a new user
 git config user.name "pentaho"
@@ -14,10 +17,10 @@ git config user.email "pentaho@pentaho.com"
 # files present with the commit message.
 git add .
 git commit -m "Pentaho Angular Toolkit Module $TRAVIS_BRANCH"
-git tag -a "$TRAVIS_BRANCH" -m "Release of version $TRAVIS_BRANCH"
+#git tag -a "$TRAVIS_BRANCH" -m "Release of version $TRAVIS_BRANCH"
 
 # force push from the current repo's master
 # (all previous history will be lost, since we are overwriting it.)
 # we redirect any output to /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://$GH_TOKEN@github.com/$GH_USER/$GH_REPO-module.git" master:master > /dev/null 2>&1
-git push --tags "https://$GH_TOKEN@github.com/$GH_USER/$GH_REPO-module.git"
+git push --force --quiet "https://$GH_TOKEN@github.com/$GH_USER/$GH_REPO.git" origin master:module > /dev/null 2>&1
+#git push --tags "https://$GH_TOKEN@github.com/$GH_USER/$GH_REPO.git"
