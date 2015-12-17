42d925bbdfbe70a2b447c2d2eada776cdd1c9f97
francisco-guilherme <francisco.guilherme@webdetails.pt>
2015-12-17 10:50:21 +0000

    Reverte to initial deploy strategy to push do different branch

--- _scripts/deploy-module.sh
+++ _scripts/deploy-module.sh
@@ -4,6 +4,7 @@ set -e # exit with nonzero exit code if anything fails
 # go to the module directory and create a *new* Git repo
 cd "$GH_REPO"
 git init
+git remote add origin "https://github.com/$GH_USER/$GH_REPO-module.git"
 
 # inside this git repo we'll pretend to be a new user
 git config user.name "pentaho"
@@ -13,8 +14,10 @@ git config user.email "pentaho@pentaho.com"
 # files present with the commit message.
 git add .
 git commit -m "Pentaho Angular Toolkit Module $TRAVIS_BRANCH"
+git tag -a "$TRAVIS_BRANCH" -m "Release of version $TRAVIS_BRANCH"
 
 # force push from the current repo's master
 # (all previous history will be lost, since we are overwriting it.)
 # we redirect any output to /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://$GH_TOKEN@github.com/$GH_USER/$GH_REPO.git" master:module > /dev/null 2>&1
+git push --force --quiet "https://$GH_TOKEN@github.com/$GH_USER/$GH_REPO-module.git" master:master > /dev/null 2>&1
+git push --tags "https://$GH_TOKEN@github.com/$GH_USER/$GH_REPO-module.git" 
