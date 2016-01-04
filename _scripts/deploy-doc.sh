514c9144236d09fb338c7f82ad2c4270aa344809
Gundi <goncalo.neto@webdetails.pt>
2016-01-04 19:13:03 +0000

    Added changes to doc deploy script so that story is preserved.

--- _scripts/deploy-doc.sh
+++ _scripts/deploy-doc.sh
@@ -2,10 +2,12 @@
 set -e # exit with nonzero exit code if anything fails
 
 # go to the module directory and create a *new* Git repo
-cd doc
+mkdir deploy_doc
+cd deploy_doc
 git init
 git remote add -t gh-pages -f origin "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
 git checkout gh-pages
+git rm -rf *
 
 # inside this git repo we'll pretend to be a new user
 git config user.name "pentaho"
@@ -13,6 +15,7 @@ git config user.email "pentaho@pentaho.com"
 
 # the first and only commit to this new Git repo contains all the
 # files present with the commit message.
+cp -r ../doc/* .
 git add .
 git commit -m "Pentaho Angular Toolkit Doc $TRAVIS_TAG"
 
