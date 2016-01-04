8679a69c7e14c7d069998c1be0b8d1ba87cee454
pentaho <pentaho@pentaho.com>
2016-01-04 20:32:06 +0000

    Pentaho Angular Toolkit Doc 6.0.0

---
+++ js/docs-setup.js
@@ -0,0 +1,150 @@
+NG_DOCS={
+  "sections": {
+    "api": "Pentaho Angular Toolkit"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "pat",
+      "shortName": "pat",
+      "type": "overview",
+      "moduleName": "pat",
+      "shortDescription": "The pat module is the main module for pentaho-angular-toolkit.",
+      "keywords": "aggregation analyzer api cde cdf essentially main module modules overview pat pentaho-angular-toolkit"
+    },
+    {
+      "section": "api",
+      "id": "pat.analyzer",
+      "shortName": "pat.analyzer",
+      "type": "overview",
+      "moduleName": "pat.analyzer",
+      "shortDescription": "The pat.analyzer modules includes the components",
+      "keywords": "analyzer api components https includes modules overview pat pentaho"
+    },
+    {
+      "section": "api",
+      "id": "pat.analyzer.directive:patAnalyzerView",
+      "shortName": "patAnalyzerView",
+      "type": "directive",
+      "moduleName": "pat.analyzer",
+      "shortDescription": "This directive handles the implementation details of embedding",
+      "keywords": "account analyzer api application call config configuration details directive ea embedding function handles iframes implementation issues loads object on-load pat standard supporting technology view"
+    },
+    {
+      "section": "api",
+      "id": "pat.analyzer.service:AnalyzerHelper",
+      "shortName": "AnalyzerHelper",
+      "type": "service",
+      "moduleName": "pat.analyzer",
+      "shortDescription": "The AnalyzerHelper service acts as an abstraction layer so",
+      "keywords": "abstraction acts analyzer analyzerhelper api aspects callback callbacks called config configuration container controllers controlling corresponding create deregisteronload directives directly don event frame frameid function getanalysispath getloadhandlers https identifying indentifying initialization interface layer loads map method object onload parameters pat pentaho registered registeronload registers remove removes retrieves returns service string takes triggers url view"
+    },
+    {
+      "section": "api",
+      "id": "pat.cde",
+      "shortName": "pat.cde",
+      "type": "overview",
+      "moduleName": "pat.cde",
+      "shortDescription": "The pat.cde module includes the components",
+      "keywords": "api cde components http includes module overview pat pt webdetails"
+    },
+    {
+      "section": "api",
+      "id": "pat.cde.service:CdeHelper",
+      "shortName": "CdeHelper",
+      "type": "service",
+      "moduleName": "pat.cde",
+      "shortDescription": "The CdeHelper service acts as an abstraction layer so",
+      "keywords": "$http abstraction acts api cde cdehelper controllers corresponding dashboard definition directives directly don getdashboardpath http inside interface layer method pat path pt repo repository service transforms url webdetails"
+    },
+    {
+      "section": "api",
+      "id": "pat.cdf",
+      "shortName": "pat.cdf",
+      "type": "overview",
+      "moduleName": "pat.cdf",
+      "shortDescription": "The pat.cdf module includes the components",
+      "keywords": "api cdf components http includes module overview pat pt webdetails"
+    },
+    {
+      "section": "api",
+      "id": "pat.cdf.controller:CdfDashboardController",
+      "shortName": "CdfDashboardController",
+      "type": "controller",
+      "moduleName": "pat.cdf",
+      "shortDescription": "This controller is mainly meant to be used by directives that want",
+      "keywords": "api cdf cdfhelper chaining controlled controller dash dashboard definition directives dom element fetched finishes getdashboard handler handlers inside instance instantianted interface jquery load meant method module mounted passed pat path promise register registered registry render renderdashboard rendering renders resolved service setdashboard setnewdashboard sets success"
+    },
+    {
+      "section": "api",
+      "id": "pat.cdf.directive:patCdfDashboard",
+      "shortName": "patCdfDashboard",
+      "type": "directive",
+      "moduleName": "pat.cdf",
+      "keywords": "api bind callback cdf controls dashboard deep-watch deeply definition directive ea evaluated event events expression expressions fetched fired functions map parameter parameters pat path shallowly url watch"
+    },
+    {
+      "section": "api",
+      "id": "pat.cdf.service:CdfHelper",
+      "shortName": "CdfHelper",
+      "type": "service",
+      "moduleName": "pat.cdf",
+      "shortDescription": "The CdfHelper service acts as an abstraction layer so",
+      "keywords": "$http abstraction acts additionally api application assumes assynchronously cdf cdfhelper configurations controllers created dash dashboard dashboards defined definition dependencies directives directly dom don element embed embedded-capabilities embedding fetched fetches finishes getnewdashboard global handler http instance instantiated instantiates interface jquery layer link load loaded method methods module modules mounted object operations org passes pat path pentaho promise pt rely renderdashboard rendering require requirejs requirements resolved returned returns server service services stable success times users webdetails window wrapper"
+    },
+    {
+      "section": "api",
+      "id": "pat.utils",
+      "shortName": "pat.utils",
+      "type": "overview",
+      "moduleName": "pat.utils",
+      "shortDescription": "The pat.utils module includes utility components",
+      "keywords": "api components includes module modules overview pat pentaho-angular-toolkit utility utils"
+    },
+    {
+      "section": "api",
+      "id": "pat.utils.service:uuid",
+      "shortName": "uuid",
+      "type": "function",
+      "moduleName": "pat.utils",
+      "shortDescription": "Uuid generator. It is declared as a constant, so it can",
+      "keywords": "api constant declared function generated generator injected pat providers service services string utils uuid"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js",
+      "http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js",
+      "http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular-animate.min.js"
+    ],
+    "styles": [],
+    "title": "Pentaho Angular Toolkit",
+    "html5Mode": true,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "loadDefaults": {
+      "angular": true,
+      "angularAnimate": true,
+      "marked": true
+    }
+  },
+  "html5Mode": true,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "js/angular.min.js",
+    "js/angular-animate.min.js",
+    "js/marked.js",
+    "http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js",
+    "http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular-animate.min.js"
+  ]
+};
